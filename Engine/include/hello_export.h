7d42cbc5bd876421bb3ef49dffa3472bb5bdccdb
victoriazakharo <vicktoriazakharo@gmail.com>
2016-08-22 20:06:21 +0300

    Library export - auto config with CMake

--- Engine/include/hello_export.h
+++ Engine/include/hello_export.h
@@ -1,6 +1,41 @@
-#pragma once
-#ifdef USE_PLATFORM_WIN32_KHR
-#define HELLO_ENGINE_API __declspec(dllexport) 
+
+#ifndef HELLO_ENGINE_API_H
+#define HELLO_ENGINE_API_H
+
+#ifdef HELLOENGINE_STATIC_DEFINE
+#  define HELLO_ENGINE_API
+#  define HELLOENGINE_NO_EXPORT
 #else
-#define HELLO_ENGINE_API 
+#  ifndef HELLO_ENGINE_API
+#    ifdef Engine_EXPORTS
+        /* We are building this library */
+#      define HELLO_ENGINE_API __declspec(dllexport)
+#    else
+        /* We are using this library */
+#      define HELLO_ENGINE_API __declspec(dllimport)
+#    endif
+#  endif
+
+#  ifndef HELLOENGINE_NO_EXPORT
+#    define HELLOENGINE_NO_EXPORT 
+#  endif
+#endif
+
+#ifndef HELLOENGINE_DEPRECATED
+#  define HELLOENGINE_DEPRECATED __declspec(deprecated)
+#endif
+
+#ifndef HELLOENGINE_DEPRECATED_EXPORT
+#  define HELLOENGINE_DEPRECATED_EXPORT HELLO_ENGINE_API HELLOENGINE_DEPRECATED
+#endif
+
+#ifndef HELLOENGINE_DEPRECATED_NO_EXPORT
+#  define HELLOENGINE_DEPRECATED_NO_EXPORT HELLOENGINE_NO_EXPORT HELLOENGINE_DEPRECATED
+#endif
+
+#define DEFINE_NO_DEPRECATED 0
+#if DEFINE_NO_DEPRECATED
+# define HELLOENGINE_NO_DEPRECATED
+#endif
+
 #endif
